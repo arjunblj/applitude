1e85f9fdeafb5057c4fb18b05e5b43e5d8081bde
Luke Van Seters <lukevanseters@gmail.com>
2014-02-09 01:47:30 -0500

    Adding client software

---
+++ client_color_sound.js
@@ -0,0 +1,153 @@
+var audioContext = new AudioContext();
+var analyser = null;
+var numElem;
+var valElem;
+var confidence = 0;
+var maxVal = 0;
+var ampVals = new Array();
+var currentAmpElement = 0;
+var ampSum = 0;
+var ampAvg = 0;
+var ampArrSize = 100;
+
+window.onload = function() {
+	numElem = document.getElementById( "num" );
+	valElem = document.getElementById("val");
+}
+
+function error() {
+    alert("Something is broken. I'm so sorry.");
+}
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+function gotStream(stream) {
+    // create an AudioNode from the stream.
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+    var filter = audioContext.createBiquadFilter()
+
+    // initialize filter characteristics
+    // filter.type = filter.HIGHPASS;
+    // filter.frequency.value = 5500;
+
+    // initialize analyser characteristics
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+
+    // make connections
+    mediaStreamSource.connect( analyser );
+    // mediaStreamSource.connect( filter );
+    // filter.connect(analyser);
+
+    updateAmp();
+}
+
+function toggleLiveInput() {
+    getUserMedia({audio:true}, gotStream);
+}
+
+var rafID = null;
+var tracks = null;
+var buflen = 2048;
+var buf = new Uint8Array( buflen );
+var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
+
+function autoCorrelate( buf, sampleRate ) {
+	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
+	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
+	var SIZE = 1000;
+	var best_offset = -1;
+	var best_correlation = 0;
+	var rms = 0;
+
+	confidence = 0;
+
+
+	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
+		return;  // Not enough data
+
+	for (var i=0;i<SIZE;i++) {
+		var val = (buf[i] - 128)/128;
+		rms += val*val;
+	}
+	rms = Math.sqrt(rms/SIZE);
+
+	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<SIZE; i++) {
+			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
+		}
+		correlation = 1 - (correlation/SIZE);
+		if (correlation > best_correlation) {
+			best_correlation = correlation;
+			best_offset = offset;
+		}
+	}
+	if ((rms>0.01)&&(best_correlation > 0.01)) {
+		confidence = best_correlation * rms * 10000;
+	}
+}
+
+function updateAmp( time ) {
+
+	analyser.getByteTimeDomainData( buf );
+	autoCorrelate( buf, audioContext.sampleRate );
+
+	if (confidence > maxVal){
+		maxVal = confidence;
+	}
+
+	numElem.innerHTML = confidence;
+
+	if (ampVals.length < ampArrSize){
+		ampVals.push(confidence);
+		if (ampVals.length == 1)
+			console.log("Filling array...");
+
+		if (ampVals.length == 99)
+			console.log("Finished filling array. Beginning data push.");
+	}
+	else {
+		ampSum -= ampVals[currentAmpElement]
+		ampVals[currentAmpElement] = confidence;
+		if (currentAmpElement == (ampArrSize-1)){
+			currentAmpElement = 0;
+			pushUsefulValOf(ampAvg);
+		}
+		else
+			currentAmpElement += 1;
+	}
+
+	ampSum += confidence;
+	ampAvg = ampSum/ampArrSize;
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	rafID = window.requestAnimationFrame( updateAmp );
+}
+
+function showMax(){
+	valElem.innerText = maxVal;
+}
+
+function pushUsefulValOf( avg ){
+
+	var pushVal = (avg*255/10000);
+	var url = "";
+
+	$.ajax({
+
+	});
+	
+}
