55da160bbfe8e4db45f3de4c1d3de69e9d654aa5
Arjun Balaji <arjunblj@gmail.com>
2014-02-08 23:07:57 -0500

    added project, imported from heroku

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :gemcutter
+gem 'sinatra', '1.2.6'
+gem 'redis', '2.2.0'
---
+++ app.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'sinatra'
+require 'redis'
+
+set :server, %w[thin mongrel webrick]
+
+configure do
+  redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+  uri = URI.parse(redisUri) 
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+def hex_color(red, green, blue)
+  "#%02x%02x%02x" % [red.to_i, green.to_i, blue.to_i]
+end
+
+def color_from_redis
+  red = REDIS.get "red"
+  green = REDIS.get "green"
+  blue = REDIS.get "blue"
+  @color = hex_color(red, green, blue)
+  erb :index
+end
+
+get '/' do
+  color_from_redis
+end
+
+post '/red' do
+  REDIS.set "red", params[:value]
+  color_from_redis
+end
+
+post '/green' do
+  REDIS.set "green", params[:value]
+  color_from_redis
+end
+
+post '/blue' do
+  REDIS.set "blue", params[:value]
+  color_from_redis
+end
+
+get '/red' do
+  REDIS.set "red", params[:value]
+  color_from_redis
+end
+
+get '/green' do
+  REDIS.set "green", params[:value]
+  color_from_redis
+end
+
+get '/blue' do
+  REDIS.set "blue", params[:value]
+  color_from_redis
+end
